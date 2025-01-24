f7017943a47d3ff28fea4f583d5833cfc613d619
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-01-24 15:30:46 +0000

    refactor dropFilm.c

--- dropFilm.c
+++ dropFilm.c
@@ -2,6 +2,17 @@
 # Author: Vatsal Sanjay
 # vatsalsanjay@gmail.com
 # Physics of Fluids
+
+version 2.0
+# Changelog
+- v2.0 (2025-01-24)
+  - Now, user can choose the density of the film independently of the drop.
+  - Now, user can set the surface tension of the film-air interface independently. 
+  - Repeating variables are: 1. Density of the drop, 2. Radius of the drop, 3. Surface tension of the drop-air interface. 
+  - updated how to add default values. 
+  - removed omega adaptation because this is somewhat broken in the newest basilisk version for axi cases.
+  - removed adapt_wavelet_limited as it is incompatible with newest Basilisk. If you want to implement adapt_wavelet_limited, please refer to: https://github.com/comphy-lab/adapt-wavelet-limited 
+  - introduced dirichlet(...) for setting boundary condition of f1 and f2. At high enough resolution, both f...[left] = ...; will work identically to f...[left] = dirichlet(...); However, when you have limited resolution, dirichlet(...) is better.
 */
 
 // 1 is drop
@@ -11,13 +22,11 @@
 #define FILTERED
 #include "two-phaseTF.h"
 #include "tension.h"
-#include "adapt_wavelet_limited.h"
 
 // Error tolerances
 #define fErr (1e-3)                                 // error tolerance in VOF
 #define KErr (1e-4)                                 // error tolerance in KAPPA
 #define VelErr (1e-2)                            // error tolerances in velocity
-#define OmegaErr (1e-2)                            // error tolerances in vorticity
 #define DissErr (1e-3)
 
 // air-water
@@ -31,12 +40,19 @@
 
 // boundary conditions
 u.t[left] = dirichlet(0.0);
-f1[left] = 0.0;
-f2[left] = 1.0;
+f1[left] = dirichlet(0.0);
+f2[left] = dirichlet(1.0);
+
+/*
+Older version
+- At high enough resolution, both f...[left] = ...; will work identically to f...[left] = dirichlet(...); However, when you have limited resolution, dirichlet(...) is better.
+*/
+// f1[left] = 0.0;
+// f2[left] = 1.0;
 
 int MAXlevel;
-double tmax, We, Ohd, Bo, Ohf, hf;
-#define MINlevel 3                                            // maximum level
+double tmax, We, Ohd, Bo, Ohf, hf, rhof, sigma23;
+#define MINlevel 4                                            // maximum level
 #define tsnap (0.01)
 
 int main(int argc, char const *argv[]) {
@@ -44,33 +60,68 @@ int main(int argc, char const *argv[]) {
   sprintf (comm, "mkdir -p intermediate");
   system(comm);
 
-  if (argc < 8){
-    fprintf(ferr, "Lack of command line arguments. Check! Need %d more arguments\n",8-argc);
+  // Default values for all parameters
+  int MAXlevel = 8;  // Default maximum refinement level
+  double tmax = 1.0;  // Default simulation end time
+  double We = 4.0;   // Default Weber number
+  double Ohd = 1e-2; // Default Ohnesorge number (drop)
+  double Bo = 0.25;   // Default Bond number
+  double Ohf = 1e-2; // Default Ohnesorge number (film)
+  double hf = 0.25;    // Default film thickness
+  double rhof = 1.0;  // Default film density
+  double sigma23 = 1.0; // Default surface tension ratio
+
+  // Print usage if help is requested
+  if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+    fprintf(stderr, "Usage: %s [MAXlevel] [tmax] [We] [Ohd] [Bo] [Ohf] [hf] [rhof] [sigma23]\n", argv[0]);
+    fprintf(stderr, "Default values:\n");
+    fprintf(stderr, "  MAXlevel = %d\n  tmax = %g\n  We = %g\n  Ohd = %g\n  Bo = %g\n",
+            MAXlevel, tmax, We, Ohd, Bo);
+    fprintf(stderr, "  Ohf = %g\n  hf = %g\n  rhof = %g\n  sigma23 = %g\n",
+            Ohf, hf, rhof, sigma23);
+    return 0;
+  }
+
+  // Override default values with command line arguments if provided
+  if (argc > 1) MAXlevel = atoi(argv[1]);
+  if (argc > 2) tmax = atof(argv[2]);
+  if (argc > 3) We = atof(argv[3]);
+  if (argc > 4) Ohd = atof(argv[4]);
+  if (argc > 5) Bo = atof(argv[5]);
+  if (argc > 6) Ohf = atof(argv[6]);
+  if (argc > 7) hf = atof(argv[7]);
+  if (argc > 8) rhof = atof(argv[8]);
+  if (argc > 9) sigma23 = atof(argv[9]);
+
+  // Validate critical parameters
+  if (hf <= 0) {
+    fprintf(stderr, "Error: Film height (hf) must be positive. Current value: %g\n", hf);
     return 1;
   }
 
-  MAXlevel = atoi(argv[1]);
-  tmax = atof(argv[2]);
-  We = atof(argv[3]); // We is 1 for 0.167 m/s <816*0.167^2*0.00075/0.017>
-  Ohd = atof(argv[4]); // <0.000816/sqrt(816*0.017*0.00075) = 0.008>
-  Bo = atof(argv[5]); // <816*10*0.00075^2/0.017 = 0.27>
-  Ohf = atof(argv[6]);
-  hf = atof(argv[7]);
-
-  if (hf == 0){
-    fprintf(ferr, "We have a problem. Wrong code. Change code or film height.\n");
-    return 1;
-  }
-  fprintf(ferr, "Level %d tmax %g. We %g, Ohd %g, Bo %g, Ohf %3.2e, hf %3.2f\n", MAXlevel, tmax, We, Ohd, Bo, Ohf, hf);
+  fprintf(stderr, "Running simulation with:\n");
+  fprintf(stderr, "Level %d tmax %g\nWe %g, Ohd %g, Bo %g\nOhf %3.2e, hf %3.2f, rhof %g, sigma23 %g\n", 
+          MAXlevel, tmax, We, Ohd, Bo, Ohf, hf, rhof, sigma23);
 
   L0=Ldomain;
   X0=-hf; Y0=0.;
   init_grid (1 << (MINlevel));
 
-  rho1 = 1.0; mu1 = Ohd; mu2 = Ohf;
+  // drop properties.
+  rho1 = 1.0; mu1 = Ohd; 
+
+  // film properties.
+  rho2 = rhof; mu2 = Ohf;
+  
+  // air properties. Note that mu3 = 1e-5 is simply to keep the dimensionless viscosity of air negligible. You can change this value to exactly match: \eta_a/\sqrt{\rho_d\gamma R_0}. For 1 mm water drop, this will be 1.35e-4 (using silicone oil density and surface tenson in drop of 1 mm). 
   rho3 = Rho21; mu3 = 1e-5;
 
-  f1.sigma = 1.0; f2.sigma = 1.0;
+  f1.sigma = 1.0; // this is always 1. 
+  f2.sigma = sigma23; // ratio of surface tension coefficient of film-air to that of drop-air interface.
+
+  /*
+  **Note:** The above framework ensures non coalescence between drop and the film. As a result, the surface tesnion coefficient of film-drop interface is naturally f1.sigma+f2.sigma (so 1+sigma23). This is by design and please only change this if you know what you are doing.
+  */
 
   run();
 
@@ -85,7 +136,6 @@ event init(t = 0){
       u.x[] = -sqrt(We)*f1[];
       u.y[] = 0.0;
     }
-    boundary((scalar *){f1, f2, u.x, u.y});
   }
 }
 
@@ -97,18 +147,13 @@ event acceleration(i++) {
   }
 }
 
-int refRegion(double x, double y, double z){
-  return (x > -hf && x < 0.1 && y < 2.0 ? MAXlevel+1 : MAXlevel);
-}
 
 event adapt(i++){
-  scalar KAPPA1[], KAPPA2[], omega[];
-  vorticity (u, omega);
+  scalar KAPPA1[], KAPPA2[];
   curvature(f1, KAPPA1);
   curvature(f2, KAPPA2);
   scalar D2c[];
   foreach(){
-    omega[] *= (f1[]+f2[]);
     double D11 = (u.y[0,1] - u.y[0,-1])/(2*Delta);
     double D22 = (u.y[]/max(y,1e-20));
     double D33 = (u.x[1,0] - u.x[-1,0])/(2*Delta);
@@ -116,10 +161,9 @@ event adapt(i++){
     double D2 = (sq(D11)+sq(D22)+sq(D33)+2.0*sq(D13));
     D2c[] = (f1[]*Ohd+f2[]*Ohf)*D2;
   }
-  boundary((scalar *){D2c, omega, KAPPA1, KAPPA2});
-  adapt_wavelet_limited ((scalar *){f1, f2, KAPPA1, KAPPA2, u.x, u.y, omega, D2c},
-     (double[]){fErr, fErr, KErr, KErr, VelErr, VelErr, OmegaErr, DissErr},
-      refRegion, MINlevel);
+  adapt_wavelet ((scalar *){f1, f2, KAPPA1, KAPPA2, u.x, u.y, D2c},
+     (double[]){fErr, fErr, KErr, KErr, VelErr, VelErr, DissErr},
+      MAXlevel, MINlevel);
 }
 // Outputs
 // static
